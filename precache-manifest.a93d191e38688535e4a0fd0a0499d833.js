20f3a4f1fc31c320f1bc220cacc108443ec1018f
Andr <ot.andriy447@gmail.com>
2020-02-06 16:21:51 +0200

    gh-pages add

---
+++ precache-manifest.a93d191e38688535e4a0fd0a0499d833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa311d7b24e9a72779110b6a71cef5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f29956dbce8e7ad7ca3",
+    "url": "/static/css/main.33ed3767.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f29956dbce8e7ad7ca3",
+    "url": "/static/js/main.7f8a658b.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
